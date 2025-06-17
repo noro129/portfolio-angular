a0c86b41cabcd581b59893ffc364089349238fdd
ERRAZI <errazi111@gmail.com>
2025-06-17 18:49:22 +0100

    ability to move folders window

--- src/app/components/folder/folder.component.ts
+++ src/app/components/folder/folder.component.ts
@@ -1,6 +1,6 @@
 import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FolderStructure } from '../../models/FolderStructure';
 
 @Component({
@@ -9,13 +9,17 @@ import { FolderStructure } from '../../models/FolderStructure';
   templateUrl: './folder.component.html',
   styleUrl: './folder.component.scss'
 })
-export class FolderComponent implements OnInit{
+export class FolderComponent implements OnInit, AfterViewInit{
+  @ViewChild("folderTab") folderTab!: ElementRef<HTMLDivElement>;
   @Input() name = '';
   @Input() iconLogo = '';
   @Input() positionX = 150;
   @Input() positionY = 150;
   foldersStructureFile = "/folders-structure.json";
   foldersStructure!: FolderStructure[];
+  isDragging = false;
+  xOffset=0;
+  yOffset=0;
 
   constructor(private httpClient : HttpClient) {}
 
@@ -25,5 +29,31 @@ export class FolderComponent implements OnInit{
         this.foldersStructure = response;
       }
     })
+  }
+
+  ngAfterViewInit(): void {
+    document.addEventListener('mousemove', this.onMouseMove);
+    document.addEventListener('mouseup', this.endDrag);
+  }
+
+  startDrag(event : MouseEvent) {
+    this.isDragging = true;
+    const rect = this.folderTab.nativeElement.getBoundingClientRect();
+    this.xOffset = event.clientX - rect.left;
+    this.yOffset = event.clientY - rect.top;
+  }
+
+  endDrag = () => {
+    this.isDragging = false;
+    this.xOffset=0;
+    this.yOffset=0;
+  }
+
+  onMouseMove = (event : MouseEvent) => {
+    if(!this.isDragging) return;
+    const el = this.folderTab.nativeElement;
+    el.style.left = `${event.clientX - this.xOffset}px`;
+    el.style.top = `${event.clientY - this.yOffset}px`;
+
   }
 }
