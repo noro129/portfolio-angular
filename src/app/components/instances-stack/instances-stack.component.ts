85daecb336524f5033251c5685a0f2fa46467ee0
ERRAZI <errazi111@gmail.com>
2025-06-19 21:04:26 +0100

    optimized stack styling

--- src/app/components/instances-stack/instances-stack.component.ts
+++ src/app/components/instances-stack/instances-stack.component.ts
@@ -16,8 +16,8 @@ export class InstancesStackComponent {
   hoverStart(event : MouseEvent) {
     this.expand = true;
     const el = this.instancesStack.nativeElement;
-    const expandedHeight = 24*this.stack.length + 5*(this.stack.length-1);
-    el.style.maxHeight = `${expandedHeight}px`;
+    const expandedHeight = 10+ 24*this.stack.length + 5*(this.stack.length-1);
+    el.style.minHeight = `${expandedHeight}px`;
     el.style.transform = `translateY(-${expandedHeight-24}px)`;
   }
 
@@ -25,7 +25,7 @@ export class InstancesStackComponent {
     this.expand = false;
     const el = this.instancesStack.nativeElement;
     console.log("shrink")
-    el.style.maxHeight = "24px";
+    el.style.minHeight = "24px";
     el.style.transform = `translateY(0)`;
   }
 }
