48f456eb878b63ea01d3a30d70d587eec15ef963
ERRAZI <errazi111@gmail.com>
2025-12-28 16:25:44 +0100

    implemented playing bar movements in blodest component

--- src/app/components/blodest/blodest.component.ts
+++ src/app/components/blodest/blodest.component.ts
@@ -1,15 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { OpenInstance } from '../../models/OpenInstance';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-blodest',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './blodest.component.html',
   styleUrl: './blodest.component.scss'
 })
-export class BlodestComponent {
+export class BlodestComponent{
   @Input() instanceData!: OpenInstance;
+  @ViewChild("gameBall", { static: false }) gameBall !: ElementRef;
+  @ViewChild("gameContainer", {static: false}) gameContainer !: ElementRef;
 
-  hide() {}
-  close() {}
+
+  gameStarted : boolean = false;
+  gameOver : boolean = false;
+
+  currentScore : number = 0;
+  maxScore : number = 0;
+
+  translateX : string = "";
+
+
+  startGame() {
+    this.gameStarted = true;
+  }
+
+  onMouseMove(event : MouseEvent) {
+    if(this.gameStarted) {
+      const rect = this.gameContainer.nativeElement.getBoundingClientRect();
+      this.translateX = `translateX(${event.clientX - rect.left - rect.width*0.1}px)`;
+    }
+  }
 }
