0b6ed97cf7ff0eaaefaa9783bfad5eeb51bc6836
ERRAZI <errazi111@gmail.com>
2026-01-02 17:40:01 +0100

    implemented cmd component view

--- src/app/components/cmd/cmd.component.ts
+++ src/app/components/cmd/cmd.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { OpenInstance } from '../../models/OpenInstance';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cmd',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './cmd.component.html',
   styleUrl: './cmd.component.scss'
 })
 export class CMDComponent {
   @Input() instanceData !: OpenInstance;
+  isActive = true;
+  commands : string[] = ['help', 'whoami', 'print', 'clear', 'exit'];
+  enteredCommands : string[] = ['help'];
+  currentCommand : string = '';
+  readonly shellPrompt = 'guest@tealos:~$';
 
+  
 }
