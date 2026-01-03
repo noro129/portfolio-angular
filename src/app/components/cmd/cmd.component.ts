f170128d3ea4e68b45788af9a243806b4af12b7a
ERRAZI <errazi111@gmail.com>
2026-01-04 00:20:11 +0100

    implemented commands in cmd component

--- src/app/components/cmd/cmd.component.ts
+++ src/app/components/cmd/cmd.component.ts
@@ -13,21 +13,20 @@ import { Subscription } from 'rxjs';
 export class CMDComponent implements OnInit, OnDestroy {
   @ViewChild("commandInput") commandInput !: ElementRef;
   @Input() instanceData !: OpenInstance;
+  @Input() exit !: (key: string, key2: string) => void;
+  @Input() key1 = '';
+  @Input() key2 = '';
   isActive = true;
   focusSubscriber !: Subscription;
-  commands : string[] = ['help', 'whoami', 'print', 'clear', 'exit'];
-  commandsResultText !: Map<string,string[]>;
-  commandsResultTable !: Map<string,Map<string, string>>;
+  commandsResult : Map<string,string[]> = new Map<string, string[]>;
   enteredCommands : string[] = [];
   currentCommand : string = '';
-  readonly shellPrompt = 'guest@tealos:~$';
+  readonly shellPrompt = 'guest@tealOs:~$';
 
   constructor(private appFocusService : AppFocusService) {}
   
   ngOnInit(): void {
-    this.commandsResultText = new Map<string, string[]>;
-    this.commandsResultTable = new Map<string, Map<string, string>>;
-
+    this.setCommandResultData();
 
     setTimeout(()=>this.commandInput.nativeElement.focus(), 0);
     this.focusSubscriber = this.appFocusService.activeSubject$.subscribe(data => {
@@ -47,8 +46,84 @@ export class CMDComponent implements OnInit, OnDestroy {
 
   executeCommand(event : any) {
     event.preventDefault();
-    const command = event.target.innerText;
-    this.enteredCommands.push(command);
+    const command = event.target.innerText.trim();
+    if(command.toLocaleLowerCase() === 'clear') {
+      this.enteredCommands = [];
+    } else if (command.toLocaleLowerCase() === 'exit') {
+      this.exit(this.key1, this.key2);
+    } else {
+      this.enteredCommands.push(command);
+    }
+    
     event.target.innerText = '';
+  }
+
+  setCommandResultData() {
+    this.commandsResult.set('help', [
+                                         'Available commands:',
+                                         ' ',
+                                         'help',
+                                         '\tDisplay this manual.',
+                                         ' ',
+                                         'print <arg>',
+                                         '\tPrint user contact or profile information.',
+                                         '\targ : github | email | phone | linkedin.',
+                                         ' ',
+                                         'whoami', 
+                                         '\tShow user information (name, education, skills, languages).',
+                                         ' ',
+                                         'clear',
+                                         '\tClear the terminal screen.',
+                                         ' ',
+                                         'exit',
+                                         '\tExit the session.'
+    ]);
+
+    this.commandsResult.set('whoami', [
+                                        '_____________________________USER______________________________',
+                                        ' ',
+                                        'full_name :         Oussama Errazi',
+                                        'profile   :         Software Engineer',
+                                        '___________________________EDUCATION___________________________',
+                                        ' ',
+                                        'software engineering degree          (ENSIAS, Rabat)',
+                                        'mathematics & physics branch         (CPGE Reda Slaoui, Agadir)',
+                                        '____________________________SKILLS_____________________________',
+                                        ' ',
+                                        'Java · Python · Spring Boot · Angular · SQL · GIT · Docker ...',
+                                        '___________________________LANGUAGES___________________________',
+                                        ' ',
+                                        '                    Arabic    :     Native Language',
+                                        '                    English   :     Proficient',
+                                        '                    French    :     Fluent',
+                                        'use whoami app for more details.'
+    ]);
+
+    this.commandsResult.set('print github', ['https://github.com/noro129']);
+    this.commandsResult.set('print phone', ['+212694105029']);
+    this.commandsResult.set('print linkedin', ['https://www.linkedin.com/in/oussama-errazi/']);
+    this.commandsResult.set('print email', ['errazi111@gmail.com']);
+  }
+
+  commandToKey(command : string) {
+    if(this.commandsResult.has(command)) return command;
+    if(command.toLocaleLowerCase().startsWith('print')){
+      return command.replace(/\s+/g, " ");
+    }
+    return command;
+  }
+
+  getErrorMessage(command : string) {
+    if (command.toLocaleLowerCase().startsWith('print')) {
+      if (command.toLocaleLowerCase() === 'print') return 'missing arg for command print.'
+      const args = command.split(/\s+/g);
+      if(args.length === 2) {
+        return 'invalid argument for command print.'
+      } else {
+        return 'invalid usage for print command — expected only one argument.'
+      }
+
+    }
+    return command + ' is not recognized as an available command.'
   }
 }
