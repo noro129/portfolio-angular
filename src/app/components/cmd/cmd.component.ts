9f25ef38e4d5f9db78c93616487b37a6023b3f36
ERRAZI <errazi111@gmail.com>
2026-01-03 02:03:44 +0100

    throw error for unrecognized commands

--- src/app/components/cmd/cmd.component.ts
+++ src/app/components/cmd/cmd.component.ts
@@ -1,12 +1,12 @@
 import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { OpenInstance } from '../../models/OpenInstance';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { AppFocusService } from '../../services/app-focus.service';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cmd',
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './cmd.component.html',
   styleUrl: './cmd.component.scss'
 })
@@ -16,6 +16,8 @@ export class CMDComponent implements OnInit, OnDestroy {
   isActive = true;
   focusSubscriber !: Subscription;
   commands : string[] = ['help', 'whoami', 'print', 'clear', 'exit'];
+  commandsResultText !: Map<string,string[]>;
+  commandsResultTable !: Map<string,Map<string, string>>;
   enteredCommands : string[] = [];
   currentCommand : string = '';
   readonly shellPrompt = 'guest@tealos:~$';
@@ -23,6 +25,10 @@ export class CMDComponent implements OnInit, OnDestroy {
   constructor(private appFocusService : AppFocusService) {}
   
   ngOnInit(): void {
+    this.commandsResultText = new Map<string, string[]>;
+    this.commandsResultTable = new Map<string, Map<string, string>>;
+
+
     setTimeout(()=>this.commandInput.nativeElement.focus(), 0);
     this.focusSubscriber = this.appFocusService.activeSubject$.subscribe(data => {
       this.isActive = data;
@@ -37,5 +43,12 @@ export class CMDComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.focusSubscriber.unsubscribe();
+  }
+
+  executeCommand(event : any) {
+    event.preventDefault();
+    const command = event.target.innerText;
+    this.enteredCommands.push(command);
+    event.target.innerText = '';
   }
 }
