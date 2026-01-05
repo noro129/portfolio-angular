3d4156477b8671f6fb01b26cfec9aa0314846ff4
ERRAZI <errazi111@gmail.com>
2026-01-05 19:00:52 +0100

    changed projects slide tracker to powerpoint styled tracker

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,12 +1,14 @@
+import { NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
   @Input() projectName = '';
   @Input() projectDescription = '';
+  @Input() carousel = false;
 }
