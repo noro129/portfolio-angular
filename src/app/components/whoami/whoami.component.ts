129a085fbfb9e06ce9cb5d77cdf081a2d573f0f3
ERRAZI <errazi111@gmail.com>
2026-01-05 17:38:11 +0100

    added tabs to whoami component

--- src/app/components/whoami/whoami.component.ts
+++ src/app/components/whoami/whoami.component.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { OpenInstance } from '../../models/OpenInstance';
+import { NgFor, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-whoami',
-  imports: [],
+  imports: [NgFor, NgClass],
   templateUrl: './whoami.component.html',
   styleUrl: './whoami.component.scss'
 })
 export class WhoamiComponent {
   @Input() instanceData !: OpenInstance;
+  readonly tabs = ['User Info', 'Education', 'Skills', 'Languages'];
+  tabI =0;
+
+
+
+  onTabClick(i : number) {
+    this.tabI = i;
+  }
 }
