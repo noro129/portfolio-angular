b6234575a1ae95cbac6622aa9c4325bdc51fcf47
ERRAZI <errazi111@gmail.com>
2026-01-08 17:54:02 +0100

    apps click focus method in trash component added

--- src/app/components/trash/trash.component.ts
+++ src/app/components/trash/trash.component.ts
@@ -1,19 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { AppsObject } from '../../models/AppsObject';
-import { KeyValuePipe, NgFor } from '@angular/common';
+import { KeyValuePipe, NgFor, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-trash',
-  imports: [NgFor, KeyValuePipe],
+  imports: [NgFor, KeyValuePipe, NgClass],
   templateUrl: './trash.component.html',
   styleUrl: './trash.component.scss'
 })
 export class TrashComponent {
   @Input() deletedApps !: Map<number, AppsObject>;
+  focusedApps = 0;
 
 
   keepOrder = ()=>0;
   trackByKey (index: number, item: { key: number; value: any }) : number {
     return item.key;
+  }
+
+
+  onClick(key : number) {
+    const app = this.deletedApps.get(key);
+    if(app) {
+      app.focused = !app.focused;
+      if(app.focused) this.focusedApps++;
+      else this.focusedApps--;
+    }
   }
 }
