263c98e670b344d7ef7b5c072c4f382e1886ec3c
ERRAZI <errazi111@gmail.com>
2026-02-09 03:28:03 +0100

    file component styling

--- src/app/components/file-reader/file-reader.component.ts
+++ src/app/components/file-reader/file-reader.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { NgStyle, NgForOf, NgFor } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Experience } from '../../models/Experience';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-file-reader',
-  imports: [],
+  imports: [NgStyle, NgFor],
   templateUrl: './file-reader.component.html',
   styleUrl: './file-reader.component.scss'
 })
-export class FileReaderComponent {
+export class FileReaderComponent implements OnInit{
+  bold = false;
+  italic = false;
+  underline = false;
+  @Input() company!: string;
 
+  experience !: Experience | undefined;
+
+  constructor(private http : HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Experience[]>("./experience.json").subscribe({
+      next: (response) => {
+            for(let res of response) {
+              if (res.company === this.company) {
+                this.experience = res;
+              } 
+            }
+          },
+      error: (error) => {
+            console.log(error);
+          }
+    });
+  }
+
+  boldEffect() {
+    this.bold = !this.bold;
+  }
+
+  italicEffect() {
+    this.italic = !this.italic;
+  }
+
+  underlineEffect() {
+    this.underline = !this.underline;
+  }
 }
